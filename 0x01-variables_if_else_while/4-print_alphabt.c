a1bf1991137d6415706c2c8c050db4a313452e87
Mika-ndlovukazi <mikamongo2@gmail.com>
2023-02-20 07:21:58 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both except q and e
+ *
+ * Return - 0 (Success)
+ */
+int main (void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != "q"))
+		{
+			putchar(ch);
+		}
+		ch++
+	}
+	putchar ('n/');
+return (0);
+}
